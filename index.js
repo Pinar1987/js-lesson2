89a37e0cc7f6f3c63eeea156569e82904b15792f
Pinar1987 <pinarbildirici87@gmail.com>
2024-10-22 21:06:01 +0200

    used slice and trim method on pricefromuser

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
-let priceFromUser = prompt("Enter price", "0");
+let priceFromUserWithDollar = prompt("Enter price", "0");
+let priceFromUser = priceFromUserWithDollar.replace("$", "").trim();
 let priceInNumber = parseFloat(priceFromUser);
 let discount = (priceInNumber * 10) / 100;
 let newPrice = priceInNumber - discount;
 console.log(`New Price: $${newPrice}`);
-
-
-/* hej jag skrev koden men detta ger ett fel när jag kör det på konsolen försökt lösa men det är så nytt kunde inte klara mer. 
-Jag ville köra min kod på Html också på pris ändringen med produkter men kom inte så lång om javascript :)MVH  /*
